a565f8dc25437913009a3be3e6269f791e525b1f
Revati Suryawanshi <121937704+revati-s@users.noreply.github.com>
2023-01-05 12:39:24 +0530

    Update streamlit_app.py
    add emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header(' 🥣 🥗 🐔 🥑🍞 Breakfast Menu')
 streamlit.text('🥣 omega3 and bluberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free range Egg')
+streamlit.text('🥗  Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
