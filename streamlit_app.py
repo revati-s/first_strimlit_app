15599a8b40b04bdeebb594358a649dc20d250d14
Revati Suryawanshi <121937704+revati-s@users.noreply.github.com>
2023-01-05 12:37:44 +0530

    Update streamlit_app.py
    add emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣'omega3 and bluberry Oatmeal')
+streamlit.text('🥣 omega3 and bluberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Boiled Free range Egg')
