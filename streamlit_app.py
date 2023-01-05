0561038314addd9b282dfeeeae7dda84c866fbe1
Revati Suryawanshi <121937704+revati-s@users.noreply.github.com>
2023-01-05 12:42:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu 🥝🍇🍌🥭')
-streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗  Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔  Hard-Boiled Free-Range Egg')
+streamlit.header('🥣  Omega 3 & Blueberry Oatmeal')
+streamlit.header(' 🥗  Kale, Spinach & Rocket Smoothie')
+streamlit.header(' 🐔  Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
